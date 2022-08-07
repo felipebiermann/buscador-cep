048ec41789cd7c64787fef5a981eac85dcfdca9b
“felipebiermanngit config --global user.name “felipebiermann <biermannfelipe@gmail.com>
2022-08-07 16:41:59 -0300

    mais style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { FiSearch } from "react-icons/fi";
-
+import "./style.css";
 function App() {
   return (
     <div className="container">
@@ -10,6 +10,13 @@ function App() {
           <FiSearch size={25} color="#FFF" />
         </button>
       </div>
+      <main className="main">
+        <h2>CEP: 21910110</h2>
+        <span>Rua Doutor Claudio Luis</span>
+        <span>Complemento: apto 302</span>
+        <span>Bancarios</span>
+        <span>Rio de Janeiro - RJ</span>
+      </main>
     </div>
   );
 }
