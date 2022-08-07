fae519881001ad3613e42727f5ee419d495ee8aa
“felipebiermanngit config --global user.name “felipebiermann <biermannfelipe@gmail.com>
2022-08-07 16:32:32 -0300

    sec commit.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { FiSearch } from "react-icons/fi";
+
 function App() {
   return (
     <div className="container">
       <h1 className="title">Buscador CEP</h1>
+      <div className="containerInput">
+        <input type="text" placeholder="Digite seu cep.." />
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#FFF" />
+        </button>
+      </div>
     </div>
   );
 }
