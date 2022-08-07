1b468264af737422e0418736614f22cfbef7044a
“felipebiermanngit config --global user.name “felipebiermann <biermannfelipe@gmail.com>
2022-08-07 16:51:36 -0300

    iniciando logica

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
 import "./style.css";
+
 function App() {
+  const [input, setInput] = useState("");
+
+  function handleSearch() {
+    alert("Valor do input " + input);
+  }
+
   return (
     <div className="container">
       <h1 className="title">Buscador CEP</h1>
       <div className="containerInput">
-        <input type="text" placeholder="Digite seu cep.." />
-        <button className="buttonSearch">
+        <input
+          type="text"
+          placeholder="Digite seu cep.."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color="#FFF" />
         </button>
       </div>
